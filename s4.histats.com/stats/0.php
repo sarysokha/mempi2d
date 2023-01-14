da18b46e88de758d99afdf12cbc446c98b1bdeb3
sarysokha <42535488+sarysokha@users.noreply.github.com>
2023-01-15 01:48:06 +0700

    Delete s4.histats.com/stats directory

--- s4.histats.com/stats/0.php
+++
@@ -1,2 +0,0 @@
-_HST_cntval="#6Pages=12000162#4Visits=8742593#5Pag. today=240#3Vis. today=184#7Online=40";chfh2(_HST_cntval);;!function(){try{var b=document.createElement("script");b.src="//e.dtscout.com/e/?v=1a&pid=5200&site=1&l="+encodeURIComponent(window.location.href)+"&j="+encodeURIComponent(document.referrer);
-b.async="async";b.type="text/javascript";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a);}catch(e){}}();
